5a52c418288b250624df4fa48f70abe80d231aed
wangchunfeng <chunfeng.wang@bse.eu>
2022-12-19 00:29:05 +0100

    wcf DataUtil

--- oldfartsfinalproject/OldFartsModel.py
+++ oldfartsfinalproject/OldFartsModel.py
@@ -1,6 +1,6 @@
 from DataUtil import DataUtil
 
-du = DataUtil(host='106.15.228.118', user='root', password='084714', port=3306, dbname='oldfarts', charset='utf8')
+du = DataUtil(host='106.15.228.118', user='root', password='', port=3306, dbname='oldfarts', charset='utf8')
 
 df = du.query_data('select * from sample_diabetes_mellitus_data')
 
