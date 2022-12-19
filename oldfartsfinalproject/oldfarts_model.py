330f7f3e09a44aba62aae9c24ab5b2d223453562
wangchunfeng <chunfeng.wang@bse.eu>
2022-12-19 10:26:36 +0100

    wcf 2022-12-19

--- oldfartsfinalproject/oldfarts_model.py
+++ oldfartsfinalproject/oldfarts_model.py
@@ -1,7 +1,7 @@
-from DataUtil import DataUtil
+from data_util import DataUtil
 
 du = DataUtil(host='106.15.228.118', user='root', password='', port=3306, dbname='oldfarts', charset='utf8')
 
-df = du.query_data('select * from sample_diabetes_mellitus_data')
+df = du.datafromsql('select * from sample_diabetes_mellitus_data')
 
 df.info()
