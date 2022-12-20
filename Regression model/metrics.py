83df624b22b74102456634bda2e56508a47df378
gaurav-abs <gauravabs@gmail.com>
2022-12-20 13:32:43 +0100

    Update metrics.py
    Added doc strings

--- Regression model/metrics.py
+++ Regression model/metrics.py
@@ -5,6 +5,13 @@ import numpy as np
 
 # MAE
 def mean_absolute_error(act, pred):
+    """
+    Agrs: (act,pred)
+
+    Calculate mean absolute error by comparing the actual test data output and the model predicted ouptut.
+
+    Returns the value of MAE
+    """
     diff = pred - act
     abs_diff = np.absolute(diff)
     mean_diff = abs_diff.mean()
@@ -13,6 +20,13 @@ def mean_absolute_error(act, pred):
 
 # MSE
 def mean_squared_error(act, pred):
+     """
+    Agrs: (act,pred)
+
+    Calculate mean squared error by comparing the actual test data output and the model predicted ouptut.
+
+    Returns the value of MAE
+    """
 
    diff = pred - act
    differences_squared = diff ** 2
@@ -22,7 +36,13 @@ def mean_squared_error(act, pred):
 
 # R-squared
 def rsquared(act, pred):
-    
+    """
+    Agrs: (act,pred)
+
+    Calculate the value of R squared by comparing the actual test data output and the model predicted ouptut.
+
+    Returns the value of MAE
+    """
     y_bar = act.mean()
     ss_tot = ((act - y_bar)**2).sum()
     ss_res = ((act - pred)**2).sum()
