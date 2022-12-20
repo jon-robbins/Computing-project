1d928ac4cde2e7ac5c81c8b6758dd2026e95ee88
wangchunfeng <chunfeng.wang@bse.eu>
2022-12-20 13:03:48 +0100

    wcf 2022-12-20

--- oldfartsfinalproject/__init__.py
+++ oldfartsfinalproject/__init__.py
@@ -1,5 +1,6 @@
 from oldfartsfinalproject import data_util
 from oldfartsfinalproject import data_preprocessing
+from oldfartsfinalproject import feature_creation
 import os
 
-__all__ = ["data_util", "data_preprocessing", "os"]
+__all__ = ["data_util", "data_preprocessing", "feature_creation", "os"]
