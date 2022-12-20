81f9bd35ddf0acb77206e6894b90af78c2e9a321
wangchunfeng <chunfeng.wang@bse.eu>
2022-12-20 16:36:23 +0100

    wcf 2022-12-20

--- Regression model/output_regression.py
+++
@@ -1,9 +0,0 @@
-from sklearn.linear_model import LinearRegression
-
-
-def output_regression(df):
-    regr = LinearRegression(fit_intercept=True)
-    Y_pred_final = regr.predict(df)
-    
-    return Y_pred_final()
-
